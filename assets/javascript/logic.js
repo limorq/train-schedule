9ec13217f12dd0cc697622ce8e27d1ba7af8684f
Limor Quintal <Limor Quintal>
2017-06-07 15:19:52 -0700

    working add and remove buttons with firebase

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,5 +1,18 @@
+ // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyC_F4mZ-mu9K0i5odcTedCP26IPAT77xGc",
+    authDomain: "train-schedule-40838.firebaseapp.com",
+    databaseURL: "https://train-schedule-40838.firebaseio.com",
+    projectId: "train-schedule-40838",
+    storageBucket: "train-schedule-40838.appspot.com",
+    messagingSenderId: "557654430431"
+  };
+
+  firebase.initializeApp(config);
+
 //global variables
-var curIx = 0;
+var curRow = 0;
+var database = firebase.database();
 
 
 //global train object
@@ -17,25 +30,28 @@ var trains = [];
 
 function populateSchedule () {
 
+	//retrieve data from firebase
+	database.ref().on("value", function(snapshot){
+
 		//update row counter
-		curIx++;
+		curRow++;
 
 		var table = document.getElementById("trainLine");
 
 		//create and insert a row and cells
-		var row = table.insertRow(curIx);
+		var row = table.insertRow(curRow);
 	    var c1 = row.insertCell(0);
 	    var c2 = row.insertCell(1);
 	    var c3 = row.insertCell(2);
 	    var c4 = row.insertCell(3);
 	    var c5 = row.insertCell(4);
-	    c1.innerHTML = train.name;
-	    c2.innerHTML = train.destination;
-	    c3.innerHTML = train.frequency;
-	    c4.innerHTML = train.nextArrival;
-	    c5.innerHTML = train.minToArrival;
+	    c1.innerHTML = snapshot.val().tName;
+	    c2.innerHTML = snapshot.val().destin;
+	    c3.innerHTML = snapshot.val().freq;
+	    c4.innerHTML = snapshot.val().arrival;
+	    //c5.innerHTML = train.minToArrival;
 
-	   
+	 });  
 }
 
 $("#addTrain").click(function(event) {
@@ -47,6 +63,14 @@ $("#addTrain").click(function(event) {
 	train.destination = $("#destin").val().trim();
 	train.frequency = $("#freq").val().trim();
 	train.nextArrival = $("#trainTime").val().trim();
+
+	//set database values
+	database.ref().set({
+		tName: train.name,
+		destin: train.destination,
+		freq: train.frequency,
+		arrival: train.nextArrival
+	});
 	
 	//display on viewport
 	populateSchedule();
@@ -61,17 +85,19 @@ $("#removeTrain").click(function(event) {
 	var trainToRemove = $("#trainName").val().trim();
 
 	//grab all td elements in the schedule table
-	var tds = document.querySelectorAll("td");
+	var trs = document.querySelectorAll("tr");
 
-	//iterate over each td
-	for (var i = 0; i < tds.length; i++) {
-	  var text = tds[i].innerText;
-	  console.log(text);
-	  //check for your target text and delete is a match
-	  if (text.toLowerCase() === trainToRemove.toLowerCase()) {
-	  	document.getElementById("trainLine").deleteRow(curIx);
-	  	curIx--;
-	  }
-	}
+		//iterate over each td
+		for (var i = 0; i < trs.length; i++) {
+		  var text = trs[i].innerText.trim();
+		  console.log(trs[i]);
+		  console.log(text);
+		  console.log(trainToRemove);
+		  //check for your target text and delete is a match
+		  if (text.toLowerCase() === trainToRemove.toLowerCase()) {
+		  	document.getElementById("trainLine").deleteRow(i);
+		  	curRow--;
+		  }
+		}
 		
 });
